7b047d934d64ec3bcecf2a0f723982482793fcd7
piyushbh04 <122432570+piyushbh04@users.noreply.github.com>
2023-01-11 19:59:08 +0530

    added pandas library and fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas 
 
 streamlit.title('MY PARENTS NEW HEALTHY DINER')
 
@@ -7,6 +8,12 @@ streamlit.header('BREAKFAST MENU')
 streamlit.text('1. Oats')
 streamlit.text('2. Bananas')
 streamlit.text('3. Peanut Butter')
+
+streamlit.header('Build your own favorite Smoothie')
+my_fruit_list=pandas.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
+
 
 
 
