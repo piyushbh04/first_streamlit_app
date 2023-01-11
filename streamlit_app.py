d8c498a42a925c74c7bbfd0cd25cedb1ef703ea2
piyushbh04 <122432570+piyushbh04@users.noreply.github.com>
2023-01-11 19:56:47 +0530

    added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,9 @@ streamlit.title('MY PARENTS NEW HEALTHY DINER')
 
 streamlit.header('BREAKFAST MENU')
 
-streamlit.text('Oats')
+streamlit.text('1. Oats')
+streamlit.text('2. Bananas')
+streamlit.text('3. Peanut Butter')
+
+
 
