71f3b3f2e73e1c04c55108ea29dc2a4db45f69d9
piyushbh04 <122432570+piyushbh04@users.noreply.github.com>
2023-01-11 20:21:34 +0530

    added loc

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,11 @@ streamlit.text('3. Peanut Butter')
 streamlit.header('Build your own favorite Smoothie')
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index("Fruit")
-streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index))
+fruits_selected=streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index))
+fruits_toshow=my_fruit_list.loc(fruits_selected)
+
+
+
 streamlit.dataframe(my_fruit_list)
 
 
